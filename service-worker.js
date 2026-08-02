3705eecbdc43f5bdb312e34547941968a34c2f65
s2pidDre <dr3george01@gmail.com>
2026-08-03 01:15:00 +0800

    adjust logo size

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='ascend-discipline-v22-inapp-logo';
+const CACHE='ascend-discipline-v23-logo-size';
 const ASSETS=['./','./index.html','./css/style.css','./js/storage.js','./js/app.js','./manifest.webmanifest','./assets/icon-192.png?v=20260803','./assets/icon-512.png?v=20260803','./assets/inapp-logo.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
