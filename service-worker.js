7acfc801c24fe8252bea0629d8c240cebf2ee717
s2pidDre <dr3george01@gmail.com>
2026-08-03 00:57:00 +0800

    in-app logo

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='ascend-discipline-v21-pwa-logo';
-const ASSETS=['./','./index.html','./css/style.css','./js/storage.js','./js/app.js','./manifest.webmanifest','./assets/icon-192.png?v=20260803','./assets/icon-512.png?v=20260803'];
+const CACHE='ascend-discipline-v22-inapp-logo';
+const ASSETS=['./','./index.html','./css/style.css','./js/storage.js','./js/app.js','./manifest.webmanifest','./assets/icon-192.png?v=20260803','./assets/icon-512.png?v=20260803','./assets/inapp-logo.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match('./index.html'))));});
